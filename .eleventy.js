97d2c07cde56a1752f07e418e805c410758b8554
Jérôme OLLIVIER <54033192+Spirecool@users.noreply.github.com>
2022-03-31 17:26:41 +0200

    First commit : index.html + readme.md

---
+++ .eleventy.js
@@ -0,0 +1,10 @@
+/* Fichier de configuration */
+
+module.exports = function(eleventyConfig) {
+    return {
+        dir: {
+            input : "src",
+            output : "public"
+        }
+    }
+}
