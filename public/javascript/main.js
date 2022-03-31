4a1cfe824a8d3fb4d88cde1a63b584865fd69bcf
Jérôme OLLIVIER <54033192+Spirecool@users.noreply.github.com>
2022-03-31 22:23:08 +0200

    Add bak-to-top js and css

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -31,3 +31,36 @@ filterContainer.addEventListener("click", (event) =>{
       });
     }
   });
+
+  
+
+///////////////////////////////////////////////
+/// BOUTON RETOUR EN HAUT (back to the top) ///
+///////////////////////////////////////////////
+
+// obtenir le bouton
+let mybutton = document.getElementById("btn-back-to-the-top");
+
+// quand l'utilisateur scroll vers le bas de 40px, faire apparaître le bouton
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 40 ||
+    document.documentElement.scrollTop > 40
+  ) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+// quand l'utilisateur clique sur le bouton, scroller vers le haut du document
+
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
