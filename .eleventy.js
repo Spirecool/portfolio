8e30046ca216dfc27f78e28df62aa2ec03a79468
Jérôme OLLIVIER <54033192+Spirecool@users.noreply.github.com>
2022-03-31 19:38:21 +0200

    add directories in .eleventy.js file

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,10 @@
 /* Fichier de configuration */
 
 module.exports = function(eleventyConfig) {
+      eleventyConfig.addPassthroughCopy("./src/images");
+      eleventyConfig.addPassthroughCopy("./src/css/");
+      eleventyConfig.addPassthroughCopy("./src/javascript");
+  
     return {
         dir: {
             input : "src",
